6950dc423b1065dc566fd125bf4911e4106ef8d7
tj-commits <thefoxjones@gmail.com>
2026-01-18 16:47:44 -0600

    e

---
+++ README.md
@@ -0,0 +1,9 @@
+# string-creashaks-organzine
+
+Exports the string "creashaks organzine".
+
+```js
+const creashaksOrganzineString = require('string-creashaks-organzine')
+
+console.log(creashaksOrganzineString) // "creashaks organzine"
+```
---
+++ co.js
@@ -0,0 +1,9 @@
+const creashaks = require('string-creashaks')
+const space = require('space-string')
+const organzine = require('string-organzine')
+
+const concat = require('general-concat')
+
+const CREASHAKS_ORGANZINE_STRING = concat(creashaks, space, organzine)
+
+module.exports = CREASHAKS_ORGANZINE_STRING
